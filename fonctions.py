1770951752f2ba721d8d0e9d90728a74535dd075
Julie MAHOT <julie.mahot@outlook.fr>
2024-04-28 13:44:46 +0200

    Ajout Menu + Fonction Afficher_Matrice (amélioration noms des lignes/colonnes)

---
+++ fonctions.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+matrice = []
+def lecture_proposition(fichier):
+    m = []
+    with open(fichier, 'r') as f:
+        for line in f:
+            m.append([int(x) for x in line.split()])
+        m.pop(0) # On retire la ligne des dimensions
+        m[-1].append(sum(m[-1])) # Le problème étant équilibré on peut additionner la ligne ou la colonne, cela revient au même
+    return m
+
+def afficher_matrice(matrice):
+    print()
+    # Noms des colonnes
+    noms_colonnes = []
+    for i in range(len(matrice[0])):
+        noms_colonnes.append(f'C{i+1}')
+
+
+    # Noms des lignes
+    noms_lignes = []
+    for j in range(len(matrice)):
+        noms_lignes.append(f'P{j+1}')
+        
+    matrice=pd.DataFrame(matrice, columns=noms_colonnes, index=noms_lignes)
+    provisions = matrice.columns[-1]
+    matrice = matrice.rename(columns={provisions: 'Provisions'})
+    commandes = matrice.index[-1]
+    matrice = matrice.rename(index={commandes: 'Commandes'})
+
+    print(matrice)
+
+def decoration_affichage(message):
+    print("\n" + "#"*50 + "\n")
+    print(message)
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import pandas as pd
+from fonctions import *
 
-matrice = []
-def lecture_proposition(fichier):
-    m = []
-    with open(fichier, 'r') as f:
-        for line in f:
-            m.append([int(x) for x in line.split()])
-        m.pop(0) # On retire la ligne des dimensions
-        m[-1].append(sum(m[-1])) # Le problème étant équilibré on peut additionner la ligne ou la colonne, cela revient au même
-    return m
+while True:
+    decoration_affichage("MENU PRINCIPAL :")
+    print("1. Tester une proposition de problème de transport")
+    print("2. Quitter")
 
-matrice = lecture_proposition("./Propositions/proposition 1.txt")
-print(pd.DataFrame(matrice))
+    choix = input("Entrez votre choix : ")
+
+    if choix == "1":
+        num_proposition = int(input("Entrer le n° de proposition de problème de transport à tester (entre 1 et 12) : "))
+        if 1 <= num_proposition <= 12:
+            file_name= f'Propositions/proposition {num_proposition}.txt'
+            decoration_affichage(f'Problème de transport N°{num_proposition}: \n')
+            matrice=lecture_proposition(file_name)
+            afficher_matrice(matrice)
+
+        else:
+            print("Numéro de proposition de problème de transport invalide. Veuillez entrer un numéro entre 1 et 12.")
+    elif choix == "2":
+        print("Au revoir !")
+        break
+    else:
+        print("Choix invalide. Veuillez entrer 1 ou 2.")
