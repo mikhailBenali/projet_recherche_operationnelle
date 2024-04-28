e7f0cbcac7352cbec0328aab8f6f14247459fea5
mikhailBenali <94171094+mikhailBenali@users.noreply.github.com>
2024-04-28 16:47:42 +0200

    Calcul des pénalités

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import pandas as pd
-
-matrice = []
-def lecture_proposition(fichier):
-    m = []
-    with open(fichier, 'r') as f:
-        for line in f:
-            m.append([int(x) for x in line.split()])
-        m.pop(0) # On retire la ligne des dimensions
-        m[-1].append(sum(m[-1])) # Le problème étant équilibré on peut additionner la ligne ou la colonne, cela revient au même
-    return m
+from fonctions import *
 
 matrice = lecture_proposition("./Propositions/proposition 1.txt")
-print(pd.DataFrame(matrice))
+afficher_matrice(matrice)
+
+couts = matrice_couts(matrice)
+balas_hammer(couts)
